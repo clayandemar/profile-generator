c3a8185c050aae8e9fa68270c106504e936d655a
clayandemar <clayandemar@gmail.com>
2023-01-30 18:17:01 -0600

    Initial commit

---
+++ develop/index.js
@@ -0,0 +1,90 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const htmlTemplate = ({ name, job, id, email, github, office }) =>
+  `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>Team Profile</title>
+  </head>
+  <body>
+    <header class="p-5 mb-4 header bg-light">
+      <div class="container">
+        <h1 class="display-4">${name}</h1>
+        <h2 class="lead">Positon: ${job}.</h2>
+        <ul class="list-group">
+          <li class="list-group-item">ID: ${id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+          <li class="list-group-item">Github: <a href="https://github.com/${github}">${github}</a></li>
+          <li class="list-group-item">Office Number: ${office}</li>
+        </ul>
+      </div>
+    </header>
+  </body>
+  </html>
+`;
+inquirer.prompt([
+  {
+    type: 'input',
+    message: 'What is your name?',
+    name: 'name'
+  },
+
+  {
+    type: 'list',
+    message: 'Are you the manager?',
+    choices: ['Manager', 'Engineer', 'Intern'],
+    name: 'job'
+  },
+  {
+    type: 'input',
+    message: 'Type in your id number:',
+    name: 'id'
+  },
+  {
+    type: 'input',
+    message: 'Type in your GitHub username',
+    name: 'github'
+  },
+  {
+    type: 'input',
+    message: 'Type in your email address',
+    name: 'email'
+  },
+  {
+    type: 'input',
+    message: 'Type in your office number',
+    name: 'office'
+  },
+  {
+    type: 'list',
+    message: 'Would you like to add another team member?',
+    choices: ['Engineer', 'Intern', 'No'],
+    name: 'newMember'
+  },
+])
+  .then(function (answers) {
+    console.log(answers);
+    if (answers.newMember === 'Engineer') {
+      //TODO
+    } else if (answers.newMember === 'Intern') {
+      //TODO
+    } else if (answers.newMember === 'No') {
+      writeToFile("index.html", htmlTemplate(answers))
+    }
+  });
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your Team Profile page has been generated")
+  });
+}
+
+
